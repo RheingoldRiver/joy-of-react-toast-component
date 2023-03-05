d3724330d409a2fb8ee085cd7bb2369c4fe03969
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-03-05 00:06:32 -0600

    Ctrl+Enter to submit all

--- src/hooks/use-special-key.js
+++ src/hooks/use-special-key.js
@@ -1,17 +1,31 @@
 import { useEffect } from "react";
 
-function useSpecialKey(key, action) {
+const KEYDOWN_PROPERTIES = {
+  Control: "ctrlKey",
+  Alt: "altKey",
+  OS: "metaKey",
+  Shift: "shiftKey",
+};
+
+function useSpecialKey(key, modifiers, action) {
   useEffect(() => {
     function dismissAll(e) {
+      let hasAllModifiers = true;
       if (e.key !== key) return;
-      action();
+      for (let item of modifiers) {
+        if (!e[KEYDOWN_PROPERTIES[item]]) {
+          hasAllModifiers = false;
+        }
+      }
+      if (!hasAllModifiers) return;
+      action(e);
     }
 
     window.addEventListener("keydown", dismissAll);
     return () => {
       window.removeEventListener("keydown", dismissAll);
     };
-  }, [key, action]);
+  }, [key, action, modifiers]);
 }
 
 export default useSpecialKey;
